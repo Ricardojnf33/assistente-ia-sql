beb78b5d8c3c8ace376358783d859e0e3af669a8
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-10-29 15:41:59 -0300

    Update app_secrets.py

--- app_secrets.py
+++ app_secrets.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
-DB_USER = os.getenv("wZbJvZPzOSHhBJZGZyvZgvuf")
-DB_PASS = os.getenv("OfEF3US7EcUR1lpe3bbeCnyMrf1N+GgHE9u9Od63Hfji2IevQg3+1AMoQ4,cRpd2E.gW,TvFIZSk.+,B_MoAehStpBMAXWnvk2EI5Fw3,2ipAYxZCsADZp3Bl9TQ_55e")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
