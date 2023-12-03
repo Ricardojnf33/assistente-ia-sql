c113d9c0b7ae545c168f4944281d20891eabe1c5
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-12-03 17:28:56 -0300

    Update voice_synthesis.py

--- voice_synthesis.py
+++ voice_synthesis.py
@@ -2,6 +2,18 @@
 
 # voice_synthesis.py
 
+'''
+* Este código utiliza o cliente Python para a API do Google Text-to-Speech para converter texto em áudio.
+* sintetizar_voz_google: Esta função cria uma solicitação para a API do Google TTS com o texto fornecido, o idioma e o gênero da voz.
+* salvar_audio: Uma função para salvar o áudio gerado em um arquivo MP3.
+* GOOGLE_APPLICATION_CREDENTIALS: Você precisa definir o caminho para o arquivo de chave da API da sua conta do Google Cloud.
+
+* Configuração da API
+
+Para utilizar este script, você precisa ter uma conta Google Cloud e configurar um projeto com a API Text-to-Speech habilitada. 
+Você também precisará gerar uma chave de API e salvá-la em um arquivo JSON, o qual será referenciado no script.
+'''
+
 from google.cloud import texttospeech
 import os
 
