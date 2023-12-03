150c01b6cc49d3d0475406edcd3c6000a5abb8c8
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-12-03 17:27:11 -0300

    Update voice_synthesis.py

--- voice_synthesis.py
+++ voice_synthesis.py
@@ -1,61 +1,63 @@
+# pip install --upgrade google-cloud-texttospeech
+
 # voice_synthesis.py
 
+from google.cloud import texttospeech
 import os
-import requests
-from pydub import AudioSegment
-from io import BytesIO
 
-# Supondo que estamos usando uma API genérica de síntese de voz
-API_URL = "https://api.sintese-voz.com/v1/voz"
-API_KEY = os.getenv("API_KEY_SYNTHESIS")
+# Configuração da chave da API (supondo que a chave da API esteja definida como uma variável de ambiente)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "caminho_para_sua_chave_de_api.json"
 
-def sintetizar_voz(texto):
+def sintetizar_voz_google(texto, idioma="pt-BR", genero_voz=texttospeech.SsmlVoiceGender.NEUTRAL):
     """
-    Converte texto em áudio usando a API de síntese de voz escolhida.
+    Converte texto em áudio usando a API do Google Text-to-Speech.
 
     Args:
         texto (str): Texto a ser convertido em áudio.
+        idioma (str): Código do idioma para a síntese de voz.
+        genero_voz (google.cloud.texttospeech.SsmlVoiceGender): Gênero da voz (NEUTRAL, MALE, FEMALE).
 
     Returns:
-        AudioSegment: Objeto de áudio que pode ser reproduzido ou salvo.
+        bytes: Áudio no formato MP3.
     """
-    if not API_KEY:
-        raise ValueError("API key para síntese de voz não configurada.")
 
-    # Configura os dados de requisição para a API
-    dados = {
-        "texto": texto,
-        "formato": "mp3",
-        "velocidade": "normal",
-        "linguagem": "pt-br"
-    }
+    # Cliente da API do Google Text-to-Speech
+    cliente = texttospeech.TextToSpeechClient()
 
-    headers = {"Authorization": f"Bearer {API_KEY}"}
+    # Configuração da solicitação de síntese de voz
+    entrada_sintese = texttospeech.SynthesisInput(text=texto)
+    voz = texttospeech.VoiceSelectionParams(
+        language_code=idioma,
+        ssml_gender=genero_voz
+    )
+    config_audio = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
 
-    # Faz a requisição à API de síntese de voz
-    resposta = requests.post(API_URL, json=dados, headers=headers)
+    # Solicitação para a API
+    resposta = cliente.synthesize_speech(
+        input=entrada_sintese, 
+        voice=voz, 
+        audio_config=config_audio
+    )
 
-    if resposta.status_code == 200:
-        # Converte a resposta em áudio
-        audio_stream = BytesIO(resposta.content)
-        audio = AudioSegment.from_file(audio_stream, format="mp3")
-        return audio
-    else:
-        # Trata erros de requisição
-        raise Exception(f"Erro na síntese de voz: {resposta.status_code}")
+    # Retorna o áudio em bytes
+    return resposta.audio_content
 
-def salvar_audio(audio, caminho_arquivo):
+def salvar_audio(audio_bytes, caminho_arquivo):
     """
     Salva o áudio em um arquivo.
 
     Args:
-        audio (AudioSegment): Objeto de áudio a ser salvo.
+        audio_bytes (bytes): Bytes do áudio a ser salvo.
         caminho_arquivo (str): Caminho para salvar o arquivo de áudio.
     """
-    audio.export(caminho_arquivo, format="mp3")
+    with open(caminho_arquivo, "wb") as out:
+        out.write(audio_bytes)
 
 # Exemplo de uso
 if __name__ == "__main__":
     texto_para_sintetizar = "Olá, bem-vindo ao assistente de SQL IA."
-    audio = sintetizar_voz(texto_para_sintetizar)
-    salvar_audio(audio, "saida_audio.mp3")
+    audio_bytes = sintetizar_voz_google(texto_para_sintetizar)
+    salvar_audio(audio_bytes, "saida_audio.mp3")
+
